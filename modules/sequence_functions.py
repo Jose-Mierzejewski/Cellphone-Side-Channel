da9e88e9d464857414f9363ce2f689b5c308bd30
Jose Mierzejewski <jose.mierzejewski@gmail.com>
2024-08-15 15:34:31 -0700

    Update sequence_functions.py

--- modules/sequence_functions.py
+++ modules/sequence_functions.py
@@ -62,3 +62,36 @@ def generate_data_from_parent_folder(path_to_parent_folder):
     y = np.array(y)
 
     return X, y
+
+def thresholding_algo(y, lag, threshold, influence):
+    """
+    This code is modified from https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/56451135#56451135
+    """
+    signals = np.zeros(len(y))
+    filteredY = np.array(y)
+    avgFilter = [0]*len(y)
+    stdFilter = [0]*len(y)
+    avgFilter[lag - 1] = np.mean(y[0:lag])
+    stdFilter[lag - 1] = np.std(y[0:lag])
+    for i in range(lag, len(y)):
+        # Absolute value removed from the next line, because I only want positive peaks, not negative peaks.
+        # Original line included abs(y[i] - avgFilter[i-1])
+        # I have not explored how these "negative peaks" could impact the filteredY mean or std by not passing through the influence parameters
+        if y[i] - avgFilter[i-1] > threshold * stdFilter [i-1]:
+            if y[i] > avgFilter[i-1]:
+                signals[i] = 1
+            else:
+                signals[i] = -1
+
+            filteredY[i] = influence * y[i] + (1 - influence) * filteredY[i-1]
+            avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+            stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+        else:
+            signals[i] = 0
+            filteredY[i] = y[i]
+            avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+            stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+
+    return dict(signals = np.asarray(signals),
+                avgFilter = np.asarray(avgFilter),
+                stdFilter = np.asarray(stdFilter))
